46c3a19167f63253d4d55126b8efbedcdc4008dd
Lily <lily.padizzy@gmail.com>
2019-08-22 10:44:28 -0700

    Updates

---
+++ precache-manifest.c9afb30bf45df0fbbdeb243178437aef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992fd0759cdb790d25586984f7cda2f1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5affe46c4421f3875ce4",
+    "url": "/monsters-rolodex/static/css/main.03073221.chunk.css"
+  },
+  {
+    "revision": "350aa9b83f1e03a99420",
+    "url": "/monsters-rolodex/static/js/2.e9fbb397.chunk.js"
+  },
+  {
+    "revision": "5affe46c4421f3875ce4",
+    "url": "/monsters-rolodex/static/js/main.8b950b8c.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
